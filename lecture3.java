b989e378ffad2ddd96a41625888e22e3d82808c6
ankit000027 <sherwoodankit99@gmail.com>
2024-08-23 23:32:19 +0530

    Create lecture3.java

---
+++ lecture3.java
@@ -0,0 +1,19 @@
+public class data_type {
+    public static void main(String[]args){
+        int x = 5;
+        int y = 7;
+        System.out.println("x is: "+x);
+        System.out.println("y is: "+y);
+
+
+        byte b = 10;
+        System.out.println("B is: "+b);
+
+        int i = b;
+        System.out.println("I is: "+i);
+  
+
+    }
+
+    
+}
